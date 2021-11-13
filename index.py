c595d23b58a7ae23e36876c174c2bf151efbcc16
Romeo <kilazlove@gmail.com>
2021-11-13 22:32:39 +0800

    paste the assignment 2

--- index.py
+++ index.py
@@ -2,3 +2,18 @@
 # Redo the programs on assignment2 but now with functions that return multiple values 
 # (move all user inputs in one function)
 
+
+money = int(input("Enter the amount of money you have: "))
+apple_Price = int(input("Enter the price of an apple: "))
+
+apples_quantity = 0
+incremented_money = 0
+
+if money < 0 or apple_Price < 0:
+    print("Price or amount is out of bounds.")
+else :
+    while incremented_money <= money - apple_Price:
+        apples_quantity += 1
+        incremented_money += apple_Price
+    print("You can buy " +str(apples_quantity)+ " apples and your change is "+ str(money - incremented_money)+" pesos.")
+    
